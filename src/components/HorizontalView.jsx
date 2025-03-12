eeecabbdf4aca419f37d909c5b6fc8262e54599e
sebastianAtAeonx <sebastian.vannier@aeonx.digital>
2025-03-12 14:38:10 +0530

    hotfix

--- src/components/HorizontalView.jsx
+++ src/components/HorizontalView.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import SideBar from './SideBar';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import SideBar from "./SideBar";
+import { Outlet } from "react-router-dom";
 
 function HorizontalView({ isMobileScreen }) {
   return (
@@ -8,7 +8,7 @@ function HorizontalView({ isMobileScreen }) {
       <div className="min-w-16 max-w-[30%] max-h-full">
         <SideBar isMobileScreen={isMobileScreen} />
       </div>
-      <div className="flex grow min-w-[70%] flex-col max-h-full">
+      <div className="flex grow w-[60%] flex-col max-h-full">
         <div className="flex grow flex-col-reverse md:flex-row overflow-hidden rounded-xl bg-neutral">
           <Outlet />
         </div>
